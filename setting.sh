586ae2c54b8ea5af67f439b7d6be236056743191
arcidor <ricardo@arcidor.com>
2011-11-08 08:15:26 +0200

    Added various port numbers to be used throughout the scripts.

--- setting.sh
+++ setting.sh
@@ -24,6 +24,20 @@ setting_locale="en_GB.utf8"				# Locale for the system
 setting_location="UTC"					# Timezone
 
 ################################################################################
+# Server Properties
+################################################################################
+
+setting_port_ssh="22"					# SSH port number
+setting_port_http="80"					# HTTP port number
+setting_port_https="443"				# HTTPS port number
+setting_port_smtp="25"					# SMTP port number
+setting_port_pop="143"					# POP port number
+setting_port_imap="110"					# IMAP port number
+setting_port_mysql="3306"				# MySQL port number (external connections)
+setting_port_dns="53"					# DNS port number
+setting_port_ftp="21"					# FTP port number
+
+################################################################################
 # Network Properties
 ################################################################################
 
