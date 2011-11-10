f99f7ac6b55068d3871473f410b3327dfa3345b8
arcidor <ricardo@arcidor.com>
2011-11-10 07:36:36 +0200

    Cleanup.

--- setting.sh
+++ setting.sh
@@ -24,12 +24,12 @@ setting_locale="en_GB.utf8"				# Locale for the system
 setting_location="UTC"					# Timezone
 
 ################################################################################
-# Server Properties
+# Server Ports
 ################################################################################
 
-setting_port_ssh="22"					# SSH port number
 setting_port_http="80"					# HTTP port number
 setting_port_https="443"				# HTTPS port number
+setting_port_ssh="22"					# SSH port number
 setting_port_smtp="25"					# SMTP port number
 setting_port_pop="143"					# POP port number
 setting_port_imap="110"					# IMAP port number
@@ -46,7 +46,6 @@ setting_fqdn="zeus.arcidor.com"			# Fully Qualified Domain Name
 setting_domain="arcidor.com"			# Domain name
 setting_ip="109.74.196.160"				# Server IP
 setting_interal_ip="192.168.163.176"	# Local IP
-setting_ssh_port="2345"					# Port over which SSH will be accessed
 
 ################################################################################
 # Application Properties
@@ -63,7 +62,6 @@ setting_mysql_memory_limit="64"			# Memory limit for MySQL processes
 # System alias
 ################################################################################
 
-alias free="free -m"
 alias update="sudo aptitude update"
 alias install="sudo aptitude install"
 alias upgrade="sudo aptitude safe-upgrade"
