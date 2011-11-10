e928ac0eb79bf91759c4ea0eec4c4248489e8e3a
arcidor <ricardo@arcidor.com>
2011-11-10 07:30:39 +0200

    Added apache domain deletion fuction.

--- admin.sh
+++ admin.sh
@@ -226,6 +226,24 @@ EOF
 
 }
 
+function apache_domain_remove {
+	
+	#First disable domain and reload webserver
+	echo "Disabling $DOMAIN_ENABLED_PATH"
+	rm -rf $DOMAIN_ENABLED_PATH
+	reload_webserver
+	#Then delete all files and config files
+	echo "Removing /etc/awstats/awstats.$DOMAIN.conf"
+	rm -rf /etc/awstats/awstats.$DOMAIN.conf
+	echo "Removing $DOMAIN_PATH"
+	rm -rf $DOMAIN_PATH
+	echo "Removing $DOMAIN_CONFIG_PATH"
+	rm -rf $DOMAIN_CONFIG_PATH
+	echo "Removing /etc/logrotate.d/$LOGROTATE_FILE"
+	rm -rf /etc/logrotate.d/$LOGROTATE_FILE
+
+}
+
 function apache_enable_domain {
 
 	# Enable the domain and restart Apache
