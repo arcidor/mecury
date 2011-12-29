23a27f33bf220683953f96c8bfcfe370a3d765c0
arcidor <ricardo@arcidor.com>
2011-12-29 10:02:07 +0200

    Added loading of the system files and tested the installation and package management sections.

--- install.sh
+++ install.sh
@@ -1,1 +1,28 @@
 #!/bin/bash
+
+################################################################################
+# Load Required Files
+################################################################################
+
+source ./setting.sh
+source ./core.sh
+
+################################################################################
+# Post Installation Setup
+################################################################################
+
+#installation_upgrade_release
+#installation_locale_update
+#installation_timezone_update
+#installation_motd_clear
+
+################################################################################
+# Package Management Configuration
+################################################################################
+
+#package_management_source_configure
+#package_management_source_update
+#package_management_aptitude_install
+#package_management_aptitude_update
+package_management_notifications_install
+#package_management_essentials_install
