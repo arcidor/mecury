c29a541bebf149edef174eb5c6098225a35d8754
Ricardo Oliveira <ricardo@arcidor.com>
2012-02-14 21:50:10 +0200

    Reworked the backup script.
    Removed unneeded variables. Added backup name to mail.

--- core.sh
+++ core.sh
@@ -959,11 +959,7 @@ function backup_install {
 	cat >> /etc/cron.daily/backup.sh <<EOF
 #!/bin/sh
 (
-	# What to backup. 
-	backup_files="/home /var/spool/mail /etc /root /boot /opt"
-
-	# Where to backup to.
-	dest="/mnt/backup"
+	mkdir /mnt/backup
 
 	# Create archive filename.
 	day=$(date +%A)
@@ -971,8 +967,10 @@ function backup_install {
 	archive_file="$hostname-$day.tgz"
 
 	# Backup the files using tar.
-	tar czf $dest/$archive_file $backup_files
-) | /bin/mail -s 'Logwatch Output:' $setting_admin_email
+	tar -cf "/mnt/backup/$archive_file" /home /var/spool/mail /etc /root /boot /opt
+	
+	echo $archive_file
+) | mail -s 'Backup Output:' $setting_admin_email
 EOF
 
 	# Apply the correct permissions to the file
